24793dfa37f6dcb31cceb033d247df44e1bbfab4
rhit-rykerzhang <rykerzhang048109@gmail.com>
2022-10-09 17:39:24 -0400

    ugly login page

---
+++ App.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import pymongo
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb://433-34.csse.rose-hulman.edu:27017")
+
+@app.route('/')
+def home():
+    return 'Welcome to the Home Page'
+
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
