846b69a35b056d7337b8d3f3491b563c89c61887
rhit-rykerzhang <rykerzhang048109@gmail.com>
2022-10-19 22:28:33 -0400

    merge

--- App.py
+++ App.py
@@ -82,27 +82,16 @@ for i in range(len(attributeArray)):
 # create a map for pokemon. Key is the id (without #) and the value is an array of attributes.
 Ipokedex = Iclient.get_or_create_cache("Ipokedex")
 
-<<<<<<< HEAD
-#the create function for Ipokedex
-=======
-# the insert function for attributeNo and Ipokedex
-
-
->>>>>>> 68028577f29d4ce4dd82a6c93fc96902a520b654
+# the create function for Ipokedex
 def Iinsert(id=0, name=None, type_1=None, type_2=None, link=None, species=None, height=0, weight=0, abilities=None, training_catch_rate=0, training_base_exp=0, training_growth_rate=0, breeding_gender_male=0, breeding_gender_female=0, stats_hp=0, stats_attack=0, stats_defense=0, stats_sp_atk=0, stats_sp_def=0, stats_speed=0, stats_total=0):
     # check if id already exist
     checkoutput = Ipokedex.get(id)
     if (checkoutput != None):
         return "id already exist."
-<<<<<<< HEAD
     Ipokedex.put(id, [name, type_1, type_2, link, species, height, weight, abilities, training_catch_rate, training_base_exp, training_growth_rate, breeding_gender_male, breeding_gender_female, stats_hp, stats_attack, stats_defense, stats_sp_atk, stats_sp_def, stats_speed, stats_total])
    
 #the delete function for Ipokedex
-=======
-    Ipokedex.put(id, [name, type_1, type_2, link, species, height, weight, abilities, training_catch_rate, training_base_exp, training_growth_rate,
-                 breeding_gender_male, breeding_gender_female, stats_hp, stats_attack, stats_defense, stats_sp_atk, stats_sp_def, stats_speed, stats_total])
 
->>>>>>> 68028577f29d4ce4dd82a6c93fc96902a520b654
 
 @app.route('/favicon.ico', methods=["GET"])
 def icon():
@@ -126,6 +115,7 @@ def mainPage():
     return render_template("main.html", logo=elep, style=css, script=js)
 
 
+    
 @app.route('/mInsert/<id>/<name>/<type_1>/<type_2>', methods=["GET", "POST"])
 def insertPokemon(id=0, name=None, type_1=None, type_2=None, link=None, species=None, height=0, weight=0, abilities=None, training_catch_rate=0, training_base_exp=0, training_growth_rate=0, breeding_gender_male=0, breeding_gender_female=0, stats_hp=0, stats_attack=0, stats_defense=0, stats_sp_atk=0, stats_sp_def=0, stats_speed=0, stats_total=0):
     if request.method == "GET":
