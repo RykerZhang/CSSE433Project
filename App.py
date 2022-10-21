76982ed3d52a9b5df69f499b6b1e54fdcc172a69
Xianshun <90218462+Xianshun-Jiang@users.noreply.github.com>
2022-10-21 00:51:52 -0400

    tmp

--- App.py
+++ App.py
@@ -27,7 +27,7 @@ db = mclient['pokemon']
 attributeNo = Iclient.get_or_create_cache("attributeNo")
 # fill the attributeNo map.
 attributeArray = ["id", "name", "type_1", "type_2", "link", "species", "height", "weight", "abilities", "training_catch_rate", "breeding_gender_male",
-                  "breeding_gender_male", "breeding_gender_female", "stats_hp", "stats_attack", "stats_defense", "stats_sp_atk", "stats_sp_def", "stats_speed", "stats_total", "imageurl"]
+                  "breeding_gender_male", "breeding_gender_female", "stats_hp", "stats_attack", "stats_defense", "stats_sp_atk", "stats_sp_def", "stats_speed", "stats_total", "img"]
 for i in range(len(attributeArray)):
     attributeNo.put(i, attributeArray[i])
 # create a map for pokemon. Key is the id (without #) and the value is an array of attributes.
@@ -72,28 +72,28 @@ def allPokemon():
     re = {}
     for data in cursor:
         data.pop("_id")
-        re[data['id_nb']] = data
+        re[data['id']] = data
     return re
 
 # insert part of data to mongodb and all the data to ignite
 
 
 @ app.route('/Insert/<id>/<name>/<type_1>/<type_2>', methods=["GET", "POST"])
-def insertPokemon(id=0, name=None, type_1=None, type_2=None, link=None, species=None, height=0, weight=0, abilities=None, training_catch_rate=0, training_base_exp=0, training_growth_rate=0, breeding_gender_male=0, breeding_gender_female=0, stats_hp=0, stats_attack=0, stats_defense=0, stats_sp_atk=0, stats_sp_def=0, stats_speed=0, stats_total=0, imageurl=""):
+def insertPokemon(id=0, name=None, type_1=None, type_2=None, link=None, species=None, height=0, weight=0, abilities=None, training_catch_rate=0, training_base_exp=0, training_growth_rate=0, breeding_gender_male=0, breeding_gender_female=0, stats_hp=0, stats_attack=0, stats_defense=0, stats_sp_atk=0, stats_sp_def=0, stats_speed=0, stats_total=0, img=""):
     if request.method == "GET":
         # Ignite insert
         checkoutput = Ipokedex.get(id)
         if (checkoutput != None):
             return "id already exist."
         Ipokedex.put(id, [name, type_1, type_2, link, species, height, weight, abilities, training_catch_rate, training_base_exp, training_growth_rate,
-                     breeding_gender_male, breeding_gender_female, stats_hp, stats_attack, stats_defense, stats_sp_atk, stats_sp_def, stats_speed, stats_total, imageurl])
+                     breeding_gender_male, breeding_gender_female, stats_hp, stats_attack, stats_defense, stats_sp_atk, stats_sp_def, stats_speed, stats_total, img])
         # Mongodb insert
         data = {
             'id': id,
             'name': name,
             'type_1': type_1,
             'species': species,
-            'imageurl': imageurl
+            'img': img
         }
         i = db.pokedex.find({'id': id})
         n = db.pokedex.find({'name': name})
@@ -116,19 +116,20 @@ def insertPokemon(id=0, name=None, type_1=None, type_2=None, link=None, species=
 @ app.route('/HomeSearch/<InfoType>/<info>', methods=["GET"])
 # if the result is not found, it will return "No such result". If the result is found, it will return the result of the find_one function.
 def Search(InfoType, info):
-    output = db.pokedex.find({InfoType: info})
+    print(InfoType)
+    print(info)
+    if InfoType == "type":
+        output = db.pokedex.find({"$or": [{"type_1": info}, {"type_2": info}]})
+    else:
+        output = db.pokedex.find({InfoType: info})
     if (output == None):
         return "No such result. Please search again"
     else:
-        pokeDict = dict()
-        for i in output:
-            id = i.get("id")
-            name = i.get("name")
-            type_1 = i.get("type_1")
-            species = i.get("species")
-            imageurl = i.get("imageurl")
-            pokeDict[i] = [id, name, type_1, species, imageurl]
-        return pokeDict
+        re = {}
+        for data in output:
+            data.pop("_id")
+            re[data['id']] = data
+        return re
 
 # detailPage return an array, in the sequence of ["id", "name", "type_1", "type_2", "link", "species", "height", "weight", "abilities", "training_catch_rate", "breeding_gender_male", "breeding_gender_male", "breeding_gender_female", "stats_hp", "stats_attack", "stats_defense", "stats_sp_atk", "stats_sp_def", "stats_speed", "stats_total", "iamgeurl"]
 
@@ -143,7 +144,7 @@ def detailPage(id):
 
 
 @ app.route('/Update', methods=["GET", "POST"])
-def Update(id=0, name=None, type_1=None, type_2=None, link=None, species=None, height=0, weight=0, abilities=None, training_catch_rate=0, training_base_exp=0, training_growth_rate=0, breeding_gender_male=0, breeding_gender_female=0, stats_hp=0, stats_attack=0, stats_defense=0, stats_sp_atk=0, stats_sp_def=0, stats_speed=0, stats_total=0, imageurl=""):
+def Update(id=0, name=None, type_1=None, type_2=None, link=None, species=None, height=0, weight=0, abilities=None, training_catch_rate=0, training_base_exp=0, training_growth_rate=0, breeding_gender_male=0, breeding_gender_female=0, stats_hp=0, stats_attack=0, stats_defense=0, stats_sp_atk=0, stats_sp_def=0, stats_speed=0, stats_total=0, img=""):
     # Mongodb Update
     if (request.method == "POST"):
         db.Book.update_one(
@@ -151,7 +152,7 @@ def Update(id=0, name=None, type_1=None, type_2=None, link=None, species=None, h
             {"$set": {"name": name,
                       "type_1": type_1,
                       "species": species,
-                      "imageurl": imageurl}
+                      "img": img}
              }
         )
         n = db.pokedex.find({'name': name})
@@ -163,7 +164,7 @@ def Update(id=0, name=None, type_1=None, type_2=None, link=None, species=None, h
         if (checkoutput != None):
             Ipokedex.remove_key(id)
             Ipokedex.put(id, [name, type_1, type_2, link, species, height, weight, abilities, training_catch_rate, training_base_exp, training_growth_rate,
-                         breeding_gender_male, breeding_gender_female, stats_hp, stats_attack, stats_defense, stats_sp_atk, stats_sp_def, stats_speed, stats_total, imageurl])
+                         breeding_gender_male, breeding_gender_female, stats_hp, stats_attack, stats_defense, stats_sp_atk, stats_sp_def, stats_speed, stats_total, img])
         else:
             return "No such Id"
 
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -38,6 +38,15 @@ pokemondb.indexPageController = class {
 pokemondb.mainPageController = class {
   constructor() {
     this.init();
+    document.querySelector("#selectSearch").onclick = (event) => {
+      console.log($(this).val());
+      console.log(1);
+    };
+    document.querySelector("#searchBtn").onclick = (event) => {
+      var InfoType = document.querySelector("#selectSearch").value;
+      var info = document.querySelector("#condition").value;
+      this.search(InfoType, info);
+    };
   }
   init() {
     fetch("/getall", { method: "GET" })
@@ -46,6 +55,7 @@ pokemondb.mainPageController = class {
         for (var key in data) {
           var pokemon = data[key];
           pokemon["name"] = pokemon["name-form"].split("-")[0];
+          // pokemon["form"] = pokemon["name-form"].split("-")[1];
           var card = this.create_card(pokemon);
           document.querySelector("#main").append(card);
         }
@@ -56,6 +66,35 @@ pokemondb.mainPageController = class {
             <img src="${data.img}">
             <p class=caption>${data["name"]}</p>
         </span>`);
+  }
+  search(InfoType, info) {
+    console.log(InfoType);
+    console.log(info);
+    var node = document.querySelector("#main");
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+    if (info == "") {
+      window.alert("condition can't be null");
+    } else {
+      fetch("/HomeSearch/" + InfoType + "/" + info, { method: "GET" })
+        .then((respnse) => respnse.json())
+        .then((data) => {
+          console.log(data);
+          console.log(data == {});
+          console.log(typeof data);
+          if (data == {}) {
+            window.alert("No such result. Please search again");
+          } else {
+            for (var key in data) {
+              var pokemon = data[key];
+              pokemon["name"] = pokemon["name-form"].split("-")[0];
+              var card = this.create_card(pokemon);
+              document.querySelector("#main").append(card);
+            }
+          }
+        });
+    }
   }
 };
 
